83190e48582f8ea95c12edb82457ca5819e97fb8
FlashScript <itzflashscript@gmail.com>
2024-09-25 09:33:47 +0330

    Nice! Finished the Home-Discover Section

--- src/template/home-page/DiscoverSection.tsx
+++ src/template/home-page/DiscoverSection.tsx
@@ -11,13 +11,13 @@ const DiscoverSection = ({ locale }: { locale: string }) => {
     // language("isEnglish") === "true"
 
     return (
-        <section className={`py-10 md:py-20 |  |  | border-2 border-indigo-600`}>
-            <div className={` |  | flex flex-col items-center gap-[0.625rem] `}>
-                <h1 className={` | text-nftCustom-text text-3xl font-semibold capitalize |  | `}>
+        <section className={`py-10 md:py-16 | md:flex md:gap-8 |  | `}>
+            <div className={` |  | flex flex-col items-center gap-[0.625rem] md:gap-5 md:basis-1/2 | `}>
+                <h1 className={` | text-nftCustom-text text-3xl md:text-4xl md:leading-[120%] xl:text-[4.2rem] font-semibold capitalize |  | `}>
                     {translateDiscover("title")}
                 </h1>
 
-                <p className={` | text-nftCustom-text text-base font-normal |  | `}>
+                <p className={`md:mb-1 xl:mb-3 | text-nftCustom-text text-base xl:text-2xl font-light |  | `}>
                     {translateDiscover("description")}
                 </p>
 
@@ -62,18 +62,47 @@ const DiscoverSection = ({ locale }: { locale: string }) => {
                     type="link" 
                     href="sign-up"
                     icon="rocket-launch"
-                    className="max-[425px]:w-full max-sm:px-20 sm:px-20 md:px-0 md:w-[14rem] border-4 border-nftCustom-cta bg-nftCustom-cta hover:bg-nftCustom-background text-nftCustom-text flex justify-center items-center rounded-[20px] py-[1.1875rem] group"
+                    className="max-[425px]:w-full max-sm:px-20 sm:px-20 md:px-0 md:w-[14rem] md:py-[1rem] md:mr-auto md:mb-2 xl:mb-6 border-4 border-nftCustom-cta bg-nftCustom-cta hover:bg-nftCustom-background text-nftCustom-text flex justify-center items-center rounded-[20px] py-[1.1875rem] group"
                     textClass="text-[1rem] leading-[140%] font-medium group-hover:text-nftCustom-cta ml-3"
                 />
 
                 <DiscoverSectionCountUp />
             </div>
 
-            <div>
+            <div className={`max-md:hidden |  |  | md:basis-1/2 `}>
                 <Link 
                     href={`/${locale}/`} 
+                    className={`overflow-hidden my-6 md:my-0 xl:ml-auto xl:w-[31rem] xl:h-[31rem] | flex flex-col |  | rounded-[20px] group`}
                 >
-                    {/* <img src="" alt="" /> */}
+                    <div className={`overflow-hidden inline-block h-fit |  |  | `}>
+                        <Image 
+                            src="/space-walking.png"
+                            alt="Space Walking"
+                            width={315}
+                            height={216}
+                            className={`w-auto h-auto object-cover xl:w-full |  |  | group-hover:scale-110 duration-500 transition-transform`}
+                        />
+                    </div>
+
+                    <div className={`py-[1.375rem] px-5 space-y-[0.625rem] | bg-nftCustom-background_secondary |  | `}>
+                        <h5 className={`xl:ml-2 | text-nftCustom-text group-hover:text-nftCustom-cta text-[1.375rem] font-medium capitalize |  | main-transition-color`}>
+                            {translateDiscover("card-title")}
+                        </h5>
+
+                        <div className={`xl:ml-2 |  | flex justify-start items-center gap-3 | `}>
+                            <Image 
+                                src="/animakid-avatar.png"
+                                alt="Animakid Avatar"
+                                width={24}
+                                height={24}
+                                className={`w-aut h-auto rounded-full |  |  | `}
+                            />
+                            
+                            <span className={` | text-base font-light text-nftCustom-text |  | `}>
+                                Animakid
+                            </span>
+                        </div>
+                    </div>
                 </Link>
             </div>
         </section>
--- src/template/home-page/DiscoverSectionCountUp.tsx
+++ src/template/home-page/DiscoverSectionCountUp.tsx
@@ -10,22 +10,22 @@ const DiscoverSectionCountUp = () => {
     const translateDiscover = useTranslations('Home-Discover');
 
     return (
-        <div className={`w-full max-md:mt-6 |  | flex justify-between | border-2 border-rose-800`}>
+        <div className={`w-full max-md:mt-6 |  | flex justify-between | `}>
             {discoverNumbers.map((number) => (
                 <div
                     key={number.id}
-                    className={` |  | flex flex-col justify-center | border-2 border-yellow-500`}
+                    className={` |  | flex flex-col justify-center | `}
                 >   
-                    <span className={` | text-xl font-bold text-nftCustom-text ${spaceMono} |  | `}>
+                    <span className={` | text-xl xl:text-3xl font-bold text-nftCustom-text ${spaceMono} |  | `}>
                         <CountUp 
                             end={number.value}
-                            duration={2}
-                            delay={3}
+                            duration={3}
+                            delay={2.5}
                             />
                         k+
                     </span>
 
-                    <p className={` | text-base font-light text-nftCustom-text |  | `}>
+                    <p className={` | text-base xl:text-2xl font-light text-nftCustom-text |  | `}>
                         {translateDiscover(number.text)}
                     </p>
                 </div>
