ae47e84b19131140f2cb3276d6032fdff1e11bdd
FlashScript <itzflashscript@gmail.com>
2024-09-29 09:57:18 +0330

    HowItWorksSection is fully Completed

--- src/template/home-page/HowItWorksSection.tsx
+++ src/template/home-page/HowItWorksSection.tsx
@@ -15,21 +15,22 @@ const HowItWorksSection = () => {
 
             <div className={` |  | grid grid-cols-1 max-md:gap-y-5 md:grid-cols-3 md:gap-x-[1.875rem] | border-2 border-indigo-600`}>
                 {howItWorksInfo.map((info) => (
-                    <div key={info.id} className={`p-5 | bg-nftCustom-background_secondary | flex gap-2 | rounded-[20px]`}>
+                    <div key={info.id} className={`max-md:p-5 md:pt-2 md:px-5 md:pb-[1.875rem] xl:pt-3 xl:px-[1.875rem] | bg-nftCustom-background_secondary | flex md:flex-col justify-between items-center max-md:gap-2 md:gap-5 | rounded-[20px]`}>
                         <Image
                             src={`/${info.image}`}
                             alt={`${info.imageAlt} Icon`}
-                            width={100}
-                            height={100}
-                            className={`object-contain |  |  | `}
+                            width={250}
+                            height={250}
+                            quality={100}
+                            className={`w-[100px] h-[100px] md:w-[160px] xl:w-[250px] md:h-[160px] xl:h-[250px] object-contain |  |  | `}
                         />
 
-                        <div className={` |  |  | border-2 border-rose-900`}>
-                            <h6 className={`mb-2 | text-nftCustom-text text-base font-medium |  | `}>
+                        <div className={`md:mb-auto | md:text-center |  | border-2 border-green-600`}>
+                            <h6 className={`mb-2 | text-nftCustom-text text-base xl:text-[1.375rem] font-medium |  | `}>
                                 {info.title}
                             </h6>
 
-                            <p className={` | text-nftCustom-text text-xs font-light |  | `}>
+                            <p className={`xl:w-11/12 xl:mx-auto | text-nftCustom-text text-xs xl:text-base font-light |  | `}>
                                 {info.text}
                             </p>
                         </div>
